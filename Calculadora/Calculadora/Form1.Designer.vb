6664cc43e278e82362ce1db3f99d864f2179238f
anapaulaantunesaraujo <anapaula.antunesaraujo@outlook.com>
2020-08-23 20:32:49 -0300

    Calculadora

---
+++ Calculadora/Calculadora/Form1.Designer.vb
@@ -0,0 +1,157 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Calculadora
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnSoma = New System.Windows.Forms.Button()
+        Me.btnMultiplica = New System.Windows.Forms.Button()
+        Me.btnDiminui = New System.Windows.Forms.Button()
+        Me.btnDivide = New System.Windows.Forms.Button()
+        Me.txtNumero1 = New System.Windows.Forms.TextBox()
+        Me.txtNumero2 = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnSoma
+        '
+        Me.btnSoma.Location = New System.Drawing.Point(12, 50)
+        Me.btnSoma.Name = "btnSoma"
+        Me.btnSoma.Size = New System.Drawing.Size(75, 23)
+        Me.btnSoma.TabIndex = 0
+        Me.btnSoma.Text = "Somar"
+        Me.btnSoma.UseVisualStyleBackColor = True
+        '
+        'btnMultiplica
+        '
+        Me.btnMultiplica.Location = New System.Drawing.Point(174, 50)
+        Me.btnMultiplica.Name = "btnMultiplica"
+        Me.btnMultiplica.Size = New System.Drawing.Size(75, 23)
+        Me.btnMultiplica.TabIndex = 1
+        Me.btnMultiplica.Text = "Multiplicar"
+        Me.btnMultiplica.UseVisualStyleBackColor = True
+        '
+        'btnDiminui
+        '
+        Me.btnDiminui.Location = New System.Drawing.Point(93, 50)
+        Me.btnDiminui.Name = "btnDiminui"
+        Me.btnDiminui.Size = New System.Drawing.Size(75, 23)
+        Me.btnDiminui.TabIndex = 2
+        Me.btnDiminui.Text = "Subtrair"
+        Me.btnDiminui.UseVisualStyleBackColor = True
+        '
+        'btnDivide
+        '
+        Me.btnDivide.Location = New System.Drawing.Point(255, 50)
+        Me.btnDivide.Name = "btnDivide"
+        Me.btnDivide.Size = New System.Drawing.Size(75, 23)
+        Me.btnDivide.TabIndex = 3
+        Me.btnDivide.Text = "Dividir"
+        Me.btnDivide.UseVisualStyleBackColor = True
+        '
+        'txtNumero1
+        '
+        Me.txtNumero1.Location = New System.Drawing.Point(12, 24)
+        Me.txtNumero1.Name = "txtNumero1"
+        Me.txtNumero1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumero1.TabIndex = 4
+        Me.txtNumero1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtNumero2
+        '
+        Me.txtNumero2.Location = New System.Drawing.Point(118, 24)
+        Me.txtNumero2.Name = "txtNumero2"
+        Me.txtNumero2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNumero2.TabIndex = 5
+        Me.txtNumero2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(224, 24)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(106, 20)
+        Me.txtTotal.TabIndex = 6
+        Me.txtTotal.Text = "0"
+        Me.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(38, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Valor 1"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(146, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(40, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "Valor 2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(252, 8)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(54, 13)
+        Me.Label3.TabIndex = 9
+        Me.Label3.Text = "Valor total"
+        '
+        'Calculadora
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(341, 81)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtNumero2)
+        Me.Controls.Add(Me.txtNumero1)
+        Me.Controls.Add(Me.btnDivide)
+        Me.Controls.Add(Me.btnDiminui)
+        Me.Controls.Add(Me.btnMultiplica)
+        Me.Controls.Add(Me.btnSoma)
+        Me.Name = "Calculadora"
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnSoma As Button
+    Friend WithEvents btnMultiplica As Button
+    Friend WithEvents btnDiminui As Button
+    Friend WithEvents btnDivide As Button
+    Friend WithEvents txtNumero1 As TextBox
+    Friend WithEvents txtNumero2 As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
---
+++ Calculadora/Calculadora/Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Calculadora
+    Private Sub Label2_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub btnSoma_Click(sender As Object, e As EventArgs) Handles btnSoma.Click
+        txtTotal.Text = Convert.ToInt32(txtNumero1.Text) + Convert.ToInt32(txtNumero2.Text)
+    End Sub
+
+    Private Sub btnDiminui_Click(sender As Object, e As EventArgs) Handles btnDiminui.Click
+        txtTotal.Text = Convert.ToInt32(txtNumero1.Text) - Convert.ToInt32(txtNumero2.Text)
+    End Sub
+
+    Private Sub btnMultiplica_Click(sender As Object, e As EventArgs) Handles btnMultiplica.Click
+        txtTotal.Text = Convert.ToInt32(txtNumero1.Text) * Convert.ToInt32(txtNumero2.Text)
+    End Sub
+
+    Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
+        txtTotal.Text = Convert.ToInt32(txtNumero1.Text) / Convert.ToInt32(txtNumero2.Text)
+    End Sub
+End Class
